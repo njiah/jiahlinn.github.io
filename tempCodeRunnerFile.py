121594889ad11102c6e0e7b08675c68dc04cde4e
n3-linn <ngwe2.linn@live.uwe.ac.uk>
2023-08-04 14:01:49 +0100

    flask set up

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, session
 
-'''
-import mysql.connector
 app = Flask(__name__)
 
 
@@ -11,12 +9,3 @@ def get_connection():
                                     password = 'Ngwe2linN9+$++',
                                     database = 'ngwe2linn')
     return conn
-'''
-
-#initializing app with Flask
-app = Flask(__name__)
-#app.secret_key = 'CV Website'
-
-@app.route('/')
-def index():
-    return render_template('main.html')
